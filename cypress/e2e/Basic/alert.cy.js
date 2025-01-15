ec78794978f9d8b358d8cb2d2bc94a49ce4e2c84
Apurbokabbo <apurbokabbo1999@gmail.com>
2025-01-15 16:28:38 +0600

    alert done

--- cypress/e2e/Basic/alert.cy.js
+++ cypress/e2e/Basic/alert.cy.js
@@ -1,24 +1,51 @@
-describe("alerts" , ()=> {
+describe("alerts", () => {
+  beforeEach(() => {
+    cy.visit("https://the-internet.herokuapp.com/javascript_alerts");
+  });
+
+  let js_alert_button = "//button[contains(text(),'Click for JS Alert')]";
+  let js_confirm_button = "//button[contains(text(),'Click for JS Confirm')]";
+  let js_prompt_button = "//button[contains(text(),'Click for JS Prompt')]";
+  let result = "//p[@id='result']";
+
+  it("alert popup", () => {
+    cy.xpath(js_alert_button).click();
+    cy.wait(1000);
+    cy.xpath(result).should("have.text", "You successfully clicked an alert");
+    cy.wait(1000);
+
+    // cy.on("window:confirm", () => {
+    //   return false;
+
+    // });
+    // cy.wait(1000);
+    // cy.xpath(js_confirm_button).click()
+    // cy.wait(1000 )
+    // cy.xpath(result).should("have.text" , "You clicked: Cancel")
+
+    cy.on("window:confirm", () => {
+      return true;
+    });
+    cy.wait(1000);
+    cy.xpath(js_confirm_button).click();
+    cy.wait(1000);
+    cy.xpath(result).should("have.text", "You clicked: Ok");
+
+    // cy.window().then(function ($win) {
+    //     cy.stub($win, "prompt").returns("test text for testing.");
+    //     cy.xpath(js_prompt_button).click();
+    //   });
 
 
-    beforeEach(()=>{
-        cy.visit("https://the-internet.herokuapp.com/javascript_alerts")
+
+    cy.window().then(win =>{
+        cy.stub(win , "prompt").returns("This is a text!!!")
+        cy.contains("Click for JS Prompt").click()
     })
-
-
-    it("alert popup" , ()=> {
-
-
-
-
-    })
+    cy.xpath(result).should("have.text","You entered: This is a text!!!")
 
 
 
 
-
-
-
-
-
-})
+  });
+});
